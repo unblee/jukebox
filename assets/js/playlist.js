cf8e313f4e19a3cf6cd9afc346de3baee372b9c6
unblee <unblee@users.noreply.github.com>
2017-08-03 00:17:14 +0900

    fix timestamp display of playlist content

--- assets/js/playlist.js
+++ assets/js/playlist.js
@@ -4,7 +4,7 @@ Vue.component("playlist", {
   methods: {
     humanize_time(seconds) {
       const s = seconds % 60;
-      const m = Math.floor(seconds / 60);
+      const m = Math.floor(seconds % 3600 / 60);
       const h = Math.floor(seconds / 3600);
       const padding = num => ("00" + num).slice(-2);
       return `${padding(h)}:${padding(m)}:${padding(s)}`;
