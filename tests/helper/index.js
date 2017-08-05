7b24a91a1ce6787acedff19213f663243bb1895f
snakazawa <nakazawa.tms0314@gmail.com>
2017-08-06 05:02:37 +0900

    add store helper

--- tests/helper/index.js
+++ tests/helper/index.js
@@ -1,7 +1,13 @@
 const supertest = require('supertest');
+const storeHelper = require('./store_helper');
+
+storeHelper.clearStore();
+storeHelper.makeStub();
+
 const app = require('../../src/app');
 
 module.exports = {
   app,
-  request: supertest(app)
+  request: supertest(app),
+  storeHelper
 };
