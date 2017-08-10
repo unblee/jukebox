d899381c0725320920723ba32066ab19965e67e2
snakazawa <nakazawa.tms0314@gmail.com>
2017-08-11 05:33:03 +0900

    impl history (client side)

--- assets/js/history.js
+++ assets/js/history.js
@@ -19,30 +19,23 @@ Vue.component('history', {
       const padding = num => `00${num}`.slice(-2);
       return `${padding(h)}:${padding(m)}:${padding(s)}`;
     },
-    isNowPlayingContent(idx) {
-      return this.playlist.nowPlayingContent && this.playlist.nowPlayingIdx === idx;
-    },
-    openClearPlaylistModal() {
-      this.$refs.clearPlaylistModal.open();
-    },
-    deleteContent(index) {
-      fetch(`/playlist/${index}`, { method: 'DELETE' });
-    },
-    playMusic(index) {
-      fetch(`/player/seek/${index}`, { method: 'POST' });
-    }
-  },
-  computed: {
-    isPlaylistEmpty() {
-      return !this.playlist.contents || this.playlist.contents.length === 0;
+    async addContent(idx) {
+      try {
+        await fetch('/playlist', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify([this.history[idx].track.link])
+        });
+      } catch (e) {
+        console.error(e);
+      }
     }
   },
 
   template: `
-  <div class="history is-flex"
-    :class="{
-      'has-content': history && history.length
-    }">
+  <div class="history is-flex" :class="{'has-content': history && history.length}">
     <div class="panel scroll-view" v-show="history && history.length">
       <a v-for="(content,idx) in history" class="panel-block history-content is-paddingless"
           :title="content.track.title"
@@ -51,20 +44,21 @@ Vue.component('history', {
             <div class="column is-1 align-self-center has-text-centered is-paddingless-vertical thumbnail-wrapper">
               <img :src="content.track.thumbnailLink" class="is-block">
             </div>
-            <div class="column is-7 playlist-content-title-wrapper">
-              {{ content.track.title }}
+            <div class="column is-7 history-content-title-wrapper">
+              <span class="tag is-light play-count is-rounded">{{ content.playCount }}</span>
+              <span class="history-title">{{ content.track.title }}</span>
             </div>
             <div class="column has-text-centered is-2">
               {{ humanizeTime(content.track.lengthSeconds) }}
             </div>
             <div class="column is-1 has-text-centered align-self-center is-paddingless-vertical">
-              <a class="is-flex in-content-button copy-link-button" @click.prevent.stop="copyUrl" :data-clipboard-text="content.link">
+              <a class="is-flex in-content-button copy-link-button" @click.prevent.stop="copyUrl" :data-clipboard-text="content.track.link">
                 <i class="material-icons icon" title="Copy Link">link</i>
               </a>
             </div>
             <div class="column is-1 has-text-centered align-self-center is-paddingless-vertical">
-              <a class="is-flex in-content-button" @click.prevent.stop="deleteContent(idx)">
-                A
+              <a class="is-flex in-content-button" @click.prevent.stop="addContent(idx)">
+                <i class="material-icons icon" title="Add to Playlist">library_add</i>
               </a>
             </div>
           </div>
