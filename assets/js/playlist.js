2ad965ad1ea209a108f5e6343b0601a241a4233b
sunya9 <mugen.xyz@gmail.com>
2017-08-12 22:14:48 +0900

    Add end of div element

--- assets/js/playlist.js
+++ assets/js/playlist.js
@@ -86,24 +86,25 @@ Vue.component('playlist', {
               </div>
             </a>
           </draggable>
+        </div>
       </div>
-    </div>
     <div class="panel playlist-controller">
-    <div class="panel">
-      <div class="panel-block playlist-border-top">
-        <clear-playlist-modal ref="clearPlaylistModal"></clear-playlist-modal>
-        <button
-          title="Clear Playlist"
-          class="button playlist-clear-button is-outlined is-fullwidth is-paddingless"
-          :disabled="isPlaylistEmpty"
-          @click="openClearPlaylistModal">
-          <i class="material-icons icon">delete_sweep</i>
-        </button>
-      </div>
-      <div class="panel-block">
-        <p class="control">
-          <links-sender></links-sender>
-        </p>
+      <div class="panel">
+        <div class="panel-block playlist-border-top">
+          <clear-playlist-modal ref="clearPlaylistModal"></clear-playlist-modal>
+          <button
+            title="Clear Playlist"
+            class="button playlist-clear-button is-outlined is-fullwidth is-paddingless"
+            :disabled="isPlaylistEmpty"
+            @click="openClearPlaylistModal">
+            <i class="material-icons icon">delete_sweep</i>
+          </button>
+        </div>
+        <div class="panel-block">
+          <p class="control">
+            <links-sender></links-sender>
+          </p>
+        </div>
       </div>
     </div>
   </div>
