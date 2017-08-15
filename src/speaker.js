81932d85570de50e0c8de45c6521f718a8944a50
snakazawa <nakazawa.tms0314@gmail.com>
2017-08-15 09:27:51 +0900

    refactor speaker.start() by removing lame.decoder and moving ffmpeg from youtube.createStream() to speaker.start()

--- src/speaker.js
+++ src/speaker.js
@@ -1,9 +1,9 @@
 const speaker = require('speaker');
-const { Decoder: decoder } = require('lame');
 const EventEmitter = require('events').EventEmitter;
 const pcmVolume = require('pcm-volume');
 const AwaitLock = require('await-lock');
 const TimedStream = require('timed-stream');
+const FFmpeg = require('fluent-ffmpeg');
 const EvenizeStream = require('./util/evenize_stream');
 
 module.exports = class Speaker extends EventEmitter {
@@ -14,9 +14,11 @@ module.exports = class Speaker extends EventEmitter {
     this._volume = volume;
 
     this._stream = null;
-    this._decoder = null;
     this._pcmVolume = null;
     this._speaker = null;
+    this._timedStream = null;
+    this._ffmpeg = null;
+    this._readableFFmpeg = null;
   }
 
   get volume() {
@@ -72,42 +74,50 @@ module.exports = class Speaker extends EventEmitter {
 
   async startWithoutLock(stream) {
     return new Promise(resolve => {
+      const { sampleRate, byteDepth, channels } = this.constructor.format;
+
       this._stream = stream;
       this._stream.on('error', err => console.error('error on stream, ', err));
 
-      this._decoder = decoder();
-      this._decoder.on('error', err => console.error('error on decoder, ', err));
+      this._ffmpeg = new FFmpeg()
+        .format('s16le')
+        .audioFrequency(sampleRate)
+        .withAudioChannels(channels);
+      this._ffmpeg.on('error', err => console.error('error on ffmpeg, ', err));
 
-      this._stream.pipe(this._decoder);
-
-      this._decoder.on('format', data => {
-        this._pcmVolume = pcmVolume();
-        this._pcmVolume.setVolume(this.volume);
-        this._pcmVolume.on('error', err => console.error('error on pcmVolume, ', err));
-
-        const byteDepth = data.bitDepth / 8;
-        this._timedStream = new TimedStream({
-          rate: byteDepth * data.channels * data.sampleRate,
-          period: 10
-        });
-        this._timedStream.on('error', err => console.error('error on timedStream, ', err));
-
-        this._evenizeStream = new EvenizeStream();
-
-        this._speaker = speaker();
-        this._speaker.on('error', err => console.error('error on speaker, ', err));
-        this._speaker._format(data);
-
-        this._decoder
-          .pipe(this._timedStream)
-          .pipe(this._evenizeStream)
-          .pipe(this._pcmVolume)
-          .pipe(this._speaker)
-          .on('close', () => this.stop());
-
-        this.emit('start');
-        resolve();
+      this._timedStream = new TimedStream({
+        rate: byteDepth * channels * sampleRate,
+        period: 10
       });
+      this._timedStream.on('error', err => console.error('error on timedStream, ', err));
+      this._timedStream.pauseStream();
+
+      this._evenizeStream = new EvenizeStream();
+      this._evenizeStream.on('error', err => console.log('error on evenizeStream, ', err));
+
+      this._pcmVolume = pcmVolume();
+      this._pcmVolume.setVolume(this.volume);
+      this._pcmVolume.on('error', err => console.error('error on pcmVolume, ', err));
+
+      this._speaker = speaker();
+      this._speaker.on('error', err => console.error('error on speaker, ', err));
+
+      this._readableFFmpeg = this._ffmpeg
+        .input(this._stream)
+        .pipe()
+        .on('error', err => console.error('error on readableFFmpeg, ', err))
+        .once('data', () => {
+          this._readableFFmpeg
+            .pipe(this._timedStream)
+            .pipe(this._evenizeStream)
+            .pipe(this._pcmVolume)
+            .pipe(this._speaker)
+            .on('close', () => this.stop());
+          this._timedStream.resumeStream();
+
+          this.emit('start');
+          resolve();
+        });
     });
   }
 
@@ -123,11 +133,20 @@ module.exports = class Speaker extends EventEmitter {
 
   stopWithoutLock() {
     if (this._stream) {
-      this._stream.end();
+      this._stream.destroy();
     }
     if (this._timedStream) {
       this._timedStream.destroy();
     }
     this.emit('stopped');
+  }
+
+  static get format() {
+    return {
+      sampleRate: 44100,
+      bitDepth: 16,
+      byteDepth: 16 / 2,
+      channels: 2
+    };
   }
 };
